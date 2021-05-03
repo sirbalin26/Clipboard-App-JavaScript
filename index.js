3bc80112ca9e4ed01a43a2ebf38d34a1ec3204e8
Sirb Alin Cristian <52006986+sirbalin26@users.noreply.github.com>
2021-05-03 20:43:38 +0300

    clipboard v 1.0

---
+++ index.js
@@ -0,0 +1,9 @@
+function clipboard(button) {
+	 let copyClipboard = document.getElementById('input');
+  
+   copyClipboard.select();
+   copyClipboard.setSelectionRange(0, 99999);
+  
+   document.execCommand('copy');
+   alert('Text copyed is:' + copyClipboard.value);
+}
